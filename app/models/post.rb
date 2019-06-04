00c919a4cef112606c6ef2e4b4b26c46ebdf2771
Joshua Tan <jtsarvida@gmail.com>
2019-06-04 06:42:15 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -16,7 +16,7 @@ class Post < ActiveRecord::Base
   
   def clickbait?
     CLICKBAIT.each do |i| 
-      if i.include?(title)
+      if i.include?(self.title)
         return true
       else
         errors.add(:title, "Not enough clickbait!")
